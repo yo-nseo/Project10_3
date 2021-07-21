9049e6d275daba94cf22b91bcc0668d9dedbed37
YoonSeo4618 <w2035@e-mirim.hs.kr>
2021-07-21 10:04:23 +0900

    completion commit

--- app/src/main/java/kr/hs/emirim/ohyoonseo/project10_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project10_3/MainActivity.java
@@ -2,7 +2,12 @@ package kr.hs.emirim.ohyoonseo.project10_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Activity Life Cycle: ", "Call OnCreate()");
+        Button btnCall = findViewById(R.id.btn_call);
+        Button btnFinish = findViewById(R.id.btn_finish);
+
+        btnCall.setOnClickListener(btnListener);
+        btnFinish.setOnClickListener(btnListener);
+    }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_call:
+                    Uri uri = Uri.parse("tel:01022223333");
+                    Intent  intent = new Intent(Intent.ACTION_DIAL, uri);
+                    startActivity(intent);
+                    break;
+                case R.id.btn_finish:
+                    finish();
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity Life Cycle: ", "Call OnCreate()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Activity Life Cycle: ", "Call OnCreate()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity Life Cycle: ", "Call OnCreate()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity Life Cycle: ", "Call OnCreate()");
     }
 }
